dc6fd83728ceb2cefd2b53dc62d89ce1b58ab773
Yann Stepienik <azukaar@gmail.com>
2017-07-26 19:46:22 +0100

    missing comma

--- src/statements/sensors.js
+++ src/statements/sensors.js
@@ -15,13 +15,12 @@ export default class FunctionStatement {
 
     run(context = {}) {
         const match = this.command.match(/__SENSOR__\s*?([a-zA-Z0-9\=\|\\\#\.\s]+)/);
-        console.log(match)
         let regexAssociated = match[1];
         
         const body = new Compiler(this.children).run(Object.assign({}, context));
 
         if(match) {
-            return `cherry__addSensor(/${regexAssociated.replace(/ /, '')}/, function() {let __result = ""; ${body}; return __result; })`;
+            return `cherry__addSensor(/${regexAssociated.replace(/ /, '')}/, function() {let __result = ""; ${body}; return __result; });`;
         }
     }
 }
